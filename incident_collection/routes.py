e422be0d76644e7c4a52ef4d8789d78990d30473
Anthony Sazonov <jericho9378@gmail.com>
2023-08-20 02:16:07 +0300

    feat: add proper problem save method

--- incident_collection/routes.py
+++ incident_collection/routes.py
@@ -1,23 +1,35 @@
 import hashlib
-# import bencodepy
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Request
 from pydantic import BaseModel, Field, validator
 from pymongo.collection import Collection
-from pymongo.cursor import Cursor
-from bson.son import SON
 from database import db
 
 
 router = APIRouter()
 
 class Problem(BaseModel):
-    header: dict[str, str] | None = Field(None)
-    body: dict[str, str]
-    hash: str | None = Field(None)
+    header: list[dict[str, str]] | None = Field(None)
+    body: list[dict[str, str]]
+    hash: list[dict[str, str]] | None = Field(None)
 
-    @validator('header', 'body', pre=True)
-    def sort_header(cls, value: dict[str, str]):
-        return {k: value[k] for k in sorted(value)}
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "body": [
+                        {"key": "name1", "value": "value1"},
+                        {"key": "name2", "value": "value2"},
+                        {"key": "name3", "value": "value3"}
+                    ]
+                }
+            ]
+        }
+    }
+
+    # @validator('body', pre=True)
+    # def sort_header(cls, values: list[dict[str, str]]):
+    #     return sorted(values, key=lambda x: x['key']) 
+        # return {k: value[k] for k in sorted(value)}
     
 
 collection: Collection[Problem] = db.incidents
@@ -25,22 +37,44 @@ collection: Collection[Problem] = db.incidents
 
 @router.post("/problems")
 async def create_problem(problem: Problem, request: Request) -> dict[str, str]:
+    headers: list[dict[str, str]] = []
+    hashes: list[dict[str, str]] = []
+
+    for header in request.headers.items():
+        headers.append({"key": header[0], "value": header[1]})
+    
+
+    problem.header = headers
+    problem.hash = hashes
+    problem.body.sort(key=lambda x: x['key'])
+    problem.header.sort(key=lambda x: x['key'])
+
     # Get hash of data
     data_hash = hashlib.sha256(problem.model_dump_json().encode()).hexdigest()
-    problem.header = dict(request.headers)
-    problem.hash = data_hash
+    # print(request.headers)
 
+    hashes.append({"key": "hash", "value": data_hash})
     # Save data to mongodb
     collection.insert_one(problem.dict())
 
     return {"hash": data_hash}
 
 
+
 @router.post("/find")
 async def find_problems(query: dict[str, str]) -> dict[str, str]:
-    cursor = collection.find({"body.hello": "world"})
-    
-    print(list(cursor))
+    cursor = collection.find({
+        "$and": [
+        {"$or": [
+            {"header": {"$elemMatch": {"key": "host", "value": "localhost:8080"}}},
+            {"body": {"$elemMatch": {"key": "host", "value": "localhost:8080"}}}
+        ]},
+        {"$or": [
+            {"header": {"$elemMatch": {"key": "name1", "value": "value1"}}},
+            {"body": {"$elemMatch": {"key": "name1", "value": "value1"}}}
+        ]}
+    ]
+    })
 
     return {"result": "ok"}
 
