b7912aa578b23747b9154436268ee1da658170eb
Anthony Sazonov <jericho9378@gmail.com>
2023-08-20 02:16:07 +0300

    feat: add proper problem save method

--- incident_collection/database.py
+++ incident_collection/database.py
@@ -1,7 +1,8 @@
 from pymongo import mongo_client, ASCENDING, MongoClient
+from pymongo.collection import Collection
 from config import DB_HOST, DB_PORT, DB_NAME
 
 
 client: MongoClient = MongoClient(f"{DB_NAME}://{DB_HOST}:{DB_PORT}/")
 
-db = client["incidents"]
+db = client.collection
--- incident_collection/routes.py
+++ incident_collection/routes.py
@@ -1,43 +1,52 @@
 import hashlib
 # import bencodepy
-from fastapi import APIRouter, Depends
-from pydantic import BaseModel, validator
+from fastapi import APIRouter, Depends, Request
+from pydantic import BaseModel, Field, validator
+from pymongo.collection import Collection
+from pymongo.cursor import Cursor
+from bson.son import SON
+from database import db
 
 
 router = APIRouter()
 
 class Problem(BaseModel):
-    header: dict[str, str]
+    header: dict[str, str] | None = Field(None)
     body: dict[str, str]
+    hash: str | None = Field(None)
 
     @validator('header', 'body', pre=True)
     def sort_header(cls, value: dict[str, str]):
         return {k: value[k] for k in sorted(value)}
+    
+
+collection: Collection[Problem] = db.incidents
 
 
 @router.post("/problems")
-async def create_problem(problem: Problem) -> dict[str, str|Problem]:
-    # Convert json to bencode
-    # json_to_bencode = bencodepy.encode(problem.model_dump_json())
+async def create_problem(problem: Problem, request: Request) -> dict[str, str]:
     # Get hash of data
-    # data_hash = hashlib.sha256(json_to_bencode).hexdigest()
     data_hash = hashlib.sha256(problem.model_dump_json().encode()).hexdigest()
+    problem.header = dict(request.headers)
+    problem.hash = data_hash
 
-    # Сохраняем данные в базу данных
-    # database.append({"hash": data_hash, "header": problem.header, "body": problem.body})
+    # Save data to mongodb
+    collection.insert_one(problem.dict())
 
-    return {"problem": problem, "hash": data_hash}
+    return {"hash": data_hash}
 
 
 @router.post("/find")
-async def find_problems(query: dict[str, str]):
-    results = []
+async def find_problems(query: dict[str, str]) -> dict[str, str]:
+    cursor = collection.find({"body.hello": "world"})
+    
+    print(list(cursor))
 
-    return results
+    return {"result": "ok"}
 
 
 @router.get("/find2")
-async def find_problems_by_hash(h: int):
-    results = []
+async def find_problems_by_hash(h: str) -> dict[str, list[Problem]]:
+    result = list(collection.find({"hash": h}))
 
-    return results
+    return {"incidents": result}
