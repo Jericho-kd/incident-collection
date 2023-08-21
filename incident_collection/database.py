f950647b9be5e6ad26e1ddc84535ee707d1d0a74
Anthony Sazonov <jericho9378@gmail.com>
2023-08-21 15:51:27 +0300

    feat: add indexes to mongodb collection

--- incident_collection/database.py
+++ incident_collection/database.py
@@ -1,4 +1,4 @@
-from pymongo import mongo_client, ASCENDING, MongoClient
+from pymongo import MongoClient
 from pymongo.collection import Collection
 from config import DB_HOST, DB_PORT, DB_NAME
 
@@ -7,3 +7,8 @@ client: MongoClient = MongoClient(f"{DB_NAME}://{DB_HOST}:{DB_PORT}/")
 
 db = client.collection
 collection: Collection[dict[str, str | list[dict[str, str]]]] = db.incidents
+
+# Create indexes for db
+collection.create_index([("header.key", 1), ("header.value", 1)])
+collection.create_index([("body.key", 1), ("body.value", 1)])
+collection.create_index([("hash", 1)])
